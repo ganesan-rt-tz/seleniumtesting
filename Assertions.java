a395e0487f028a1f19e275d3a3faf0ae64b076c0
ganesan-rt-tz <ganesan.rt@tamilzorous.com>
2025-12-04 17:31:53 +0530

    Update Assertions.java

--- Assertions.java
+++ Assertions.java
@@ -1,30 +1,67 @@
-package Packagetest;
-
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import java.time.Duration;
 
-public class Assertions {
+public class Alert {
     public static void main(String[] args) {
 
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");   // headless mode
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
 
-        // assertTitle
-        String title = driver.getTitle();
-        System.out.println(title.equals("Web form") ? "Title OK" : "Title Not OK");
+        WebDriver driver = new ChromeDriver(options);
 
-        // type
-        driver.findElement(By.id("my-text-id")).sendKeys("Hello");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
 
-        // assertValue
-        String val = driver.findElement(By.id("my-text-id")).getAttribute("value");
-        System.out.println(val.equals("Hello") ? "Value OK" : "Value Not OK");
+        Actions act = new Actions(driver);
 
-        // assertElementPresent
-        boolean present = driver.findElements(By.cssSelector("input[type='submit']")).size() > 0;
-        System.out.println(present ? "Submit Present" : "Submit Missing");
+        try {
 
-        driver.quit();
+            driver.get("https://demoqa.com/menu");
+            WebElement hover = driver.findElement(By.xpath("//a[text()='Main Item 2']"));
+            act.moveToElement(hover).perform();
+
+            driver.get("https://demoqa.com/buttons");
+            WebElement rightClick = driver.findElement(By.id("rightClickBtn"));
+            act.contextClick(rightClick).perform();
+
+            WebElement doubleClick = driver.findElement(By.id("doubleClickBtn"));
+            act.doubleClick(doubleClick).perform();
+
+            driver.get("https://demoqa.com/droppable");
+            WebElement src = driver.findElement(By.id("draggable"));
+            WebElement tgt = driver.findElement(By.id("droppable"));
+            act.dragAndDrop(src, tgt).perform();
+
+            driver.get("https://demoqa.com/slider");
+            WebElement slider = driver.findElement(By.xpath("//input[@type='range']"));
+            act.clickAndHold(slider).moveByOffset(50, 0).release().perform();
+
+            driver.get("https://demoqa.com/text-box");
+            WebElement username = driver.findElement(By.id("userName"));
+            username.sendKeys("Ganesan");
+
+            username.sendKeys(Keys.TAB);
+            username.sendKeys(Keys.ENTER);
+
+            username.sendKeys(Keys.CONTROL + "a");
+            username.sendKeys(Keys.CONTROL + "c");
+            username.sendKeys(Keys.CONTROL + "v");
+
+            username.sendKeys(Keys.ARROW_DOWN);
+            username.sendKeys(Keys.ARROW_UP);
+
+            System.out.println("SUCCESS");
+
+        } finally {
+            driver.quit();
+        }
     }
 }
